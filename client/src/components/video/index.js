bf00212512efd56e63f71131cdc8a8dea03074c9
Lauren <TechnologicalType@MacBook-Air-2.attlocal.net>
2021-10-21 11:55:16 -0400

    added tech vide clip

--- client/src/components/video/index.js
+++ client/src/components/video/index.js
@@ -7,8 +7,11 @@ const Video = (props)=> {
         
         <div  >
         <video className='videoTag' autoPlay loop muted>
-        <source src={props.sample} type='video/mp4' />
+        <source src={props.sample}  type='video/mp4' />
     </video>
+    <video className='videoTag2'  autoPlay loop muted>
+    <source src={props.sample2}  type='video/mp4' />
+</video>
    
       
 
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -4,22 +4,33 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Video from "../../components/video";
 import video from './FromPolitical.mp4';
+import v2 from './Technology.mp4';
 const Home = () => {
   return (
     <div id="head">
-      <Container>
+      <Container fluid>
   <Row>
     <Col></Col>
     <Col>Welcome To LAW</Col>
     <Col></Col>
   </Row>
+
+
   <Row>
   <Col></Col>
   <Col></Col>
   <Col>Logic At Work</Col>
 </Row>
-<Video
-sample={video}/>
+
+<Row>
+<Col></Col>
+<Col ><Video
+sample={video}/></Col>
+<Col ><Video sample2={v2}/></Col>
+</Row>
+
+
+
 </Container>
        
       
